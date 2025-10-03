71a6eea66b0b6ddf99a9d85f54e48f2aae299ce1
S Shashank <sshashank@Ss-MacBook-Air.local>
2025-10-04 00:44:27 +0530

    Fix prisma client export

--- optical-backend/src/prismaClient.js
+++ optical-backend/src/prismaClient.js
@@ -1,9 +1,6 @@
-// src/prismaClient.js
+// prismaClient.js
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-process.on("SIGINT", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
+export default prisma;
